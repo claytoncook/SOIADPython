7a2a1f84813a12c05f881044b5f2e7950cca5e0e
Clayton Cook <claytoncook@Claytons-MacBook-Pro.local>
2018-02-22 23:40:00 -0500

    problem 4 done

--- problems/problem4.py
+++ problems/problem4.py
@@ -5,6 +5,14 @@
 # the numerical value of a single character.
 # Write your code and return the sum.
 def problem4():
-	str = "How much is this string of characters, '!#)!^$!', actually worth?"
-	
-	return ""
+	string = "How much is this string of characters, '!#)!^$!', actually worth?"
+        list_string = list(string)
+        print (list_string)
+        num_holder = 0
+        final_answer = 0
+        while num_holder < len(list_string):
+            final_answer += (ord(list_string[num_holder]))
+            num_holder += 1
+            print(final_answer)
+
+problem4()
