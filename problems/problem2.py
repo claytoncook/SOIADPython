e4690b3563c42f4821ed90b1d70e166c78451176
Clayton Cook <claytoncook@Claytons-MacBook-Pro.local>
2018-02-22 22:45:30 -0500

    solved problem 2

--- problems/problem2.py
+++ problems/problem2.py
@@ -3,5 +3,13 @@
 # Find the sum of the positive, even numbers between 0 and 100. 
 # Write your code and return the sum.
 def problem2():
-   
-	return ""
+    num_holder = 0
+    final_answer = 0
+    while num_holder < 100:
+        num_holder += 2
+        final_answer += num_holder
+        print(num_holder)
+
+    print(final_answer)
+
+problem2()
